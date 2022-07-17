8335360ea1ec21b4a304ebf8da3b188f4259466a
midflandir <juancamilo372cp@live.com>
2022-07-16 22:01:38 -0500

    minor fix on DAO classes

--- src/Domain/Ticket/Ticket.java
+++ src/Domain/Ticket/Ticket.java
@@ -1,13 +1,116 @@
 package Domain.Ticket;
 
+import java.sql.Time;
 import java.util.Date;
 
 public class Ticket {
 
     private String code;
+    private String bicycle;
+    private String User;
     private Date date;
-    private Status status;
-    private Integer TimeDebt;
+    private Time Starttime;
+    private Time Endtime;
     private Boolean helmetDebt;
-    private Integer DamagedDebt;
+    private Boolean DamagedDebt;
+    private Status status;
+    private Double Amount;
+
+    public Ticket() {
+    }
+
+    public Ticket(String code, String bicycle, String user, Date date,
+                  Time starttime, Time endtime, Boolean helmetDebt,
+                  Boolean damagedDebt, Status status, Double amount) {
+        this.code = code;
+        this.bicycle = bicycle;
+        User = user;
+        this.date = date;
+        Starttime = starttime;
+        Endtime = endtime;
+        this.helmetDebt = helmetDebt;
+        DamagedDebt = damagedDebt;
+        this.status = status;
+        Amount = amount;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getBicycle() {
+        return bicycle;
+    }
+
+    public void setBicycle(String bicycle) {
+        this.bicycle = bicycle;
+    }
+
+    public String getUser() {
+        return User;
+    }
+
+    public void setUser(String user) {
+        User = user;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public Time getStarttime() {
+        return Starttime;
+    }
+
+    public void setStarttime(Time starttime) {
+        Starttime = starttime;
+    }
+
+    public Time getEndtime() {
+        return Endtime;
+    }
+
+    public void setEndtime(Time endtime) {
+        Endtime = endtime;
+    }
+
+    public Boolean getHelmetDebt() {
+        return helmetDebt;
+    }
+
+    public void setHelmetDebt(Boolean helmetDebt) {
+        this.helmetDebt = helmetDebt;
+    }
+
+    public Boolean getDamagedDebt() {
+        return DamagedDebt;
+    }
+
+    public void setDamagedDebt(Boolean damagedDebt) {
+        DamagedDebt = damagedDebt;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public Double getAmount() {
+        return Amount;
+    }
+
+    public void setAmount(Double amount) {
+        Amount = amount;
+    }
 }
--- src/Menu/BorrowBicycle.java
+++ src/Menu/BorrowBicycle.java
@@ -1,12 +1,33 @@
 package Menu;
 import Domain.Users.user;
+
+import java.time.DayOfWeek;
 import java.util.ArrayList;
+import java.util.Scanner;
+
 import static DAO.Daousers.readusers;
 
 public class BorrowBicycle {
     private static Registeruser reguser = new Registeruser();
+    private static Scanner in = new Scanner(System.in);
     public static void start(){
+        String ID;
+        System.out.println("Please enter your ID");
+        ID = in.nextLine();
+
         ArrayList<user> Userlist = new ArrayList<user>();
         Userlist = readusers();
+
+        for (int i = 0; i < Userlist.size(); i++) {
+
+
+            if ( Userlist.get(i).getTicketdebts() > 0){
+                System.out.println("The user" + ID + "has a ticket with debt Please cancel it and try again");
+            }
+            if ( Userlist.get(i).getTicketdebts() == 0){
+
+            }
+
+        }
     }
 }
